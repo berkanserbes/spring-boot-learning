2b5725ddf5b2edea778c909f6185d3a68d13a777
Berkan Serbes <berkan_serbes@hotmail.com>
2023-08-27 00:43:47 +0300

    First demo is done

--- spring-boot-course/src/main/java/spring/course/controllers/UserController.java
+++ spring-boot-course/src/main/java/spring/course/controllers/UserController.java
@@ -1,12 +1,41 @@
 package spring.course.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring.course.model.UserModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class UserController {
-    @GetMapping("/getUser")
-    public String getUser() {
-        return "<html><body><h1>Berkan</h1></body></html>";
+    private Map<String, UserModel> userMap = new HashMap<>();
+
+    public UserController() {
+        userMap.put("Berkan", new UserModel("Berkan", "Serbes", 1));
+        userMap.put("John", new UserModel("John", "Doe", 2));
+        userMap.put("Jane", new UserModel("Jane", "Doe", 3));
+    }
+    @GetMapping("/getUser/{userName}")
+    public ResponseEntity<UserModel> getUser(@PathVariable String userName) {
+        if (userMap.containsKey(userName)) {
+            return ResponseEntity.of(Optional.of(userMap.get(userName)));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<HttpStatus> createUser(@RequestBody UserModel user) {
+        userMap.put(user.getFirstName(), user);
+        return ResponseEntity.accepted().build();
+    }
+
+    @DeleteMapping("/deleteUser/{userName}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable String userName) {
+        userMap.remove(userName);
+        return ResponseEntity.accepted().build();
     }
 }
