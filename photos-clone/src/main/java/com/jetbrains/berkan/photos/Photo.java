901753b1fe3b3a318e1ff31c37817d9b11def79b
Berkan Serbes <berkan_serbes@hotmail.com>
2023-08-27 14:46:27 +0300

    Modified files

--- photos-clone/src/main/java/com/jetbrains/berkan/photos/Photo.java
+++ photos-clone/src/main/java/com/jetbrains/berkan/photos/Photo.java
@@ -1,13 +1,20 @@
 package com.jetbrains.berkan.photos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public class Photo {
+
     private String id;
 
     @NotEmpty
     private String fileName;
+
+    private String contentType;
+
+    @JsonIgnore
+    private byte[] data;
 
     public Photo() {};
     public Photo(String id, String fileName) {
@@ -29,5 +36,21 @@ public class Photo {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
     }
 }
--- photos-clone/src/main/java/com/jetbrains/berkan/photos/PhotosController.java
+++ photos-clone/src/main/java/com/jetbrains/berkan/photos/PhotosController.java
@@ -1,41 +1,32 @@
 package com.jetbrains.berkan.photos;
 
-import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
 public class PhotosController {
 
-    private Map<String, Photo> db = new HashMap<>() {
-        {
-            put("1", new Photo("1", "hello.jpg"));
-        };
-        {
-            put("2", new Photo("2", "hello2.jpg"));
-        };
-    };
+    private final PhotosService photosService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public PhotosController(PhotosService photosService) {
+        this.photosService = photosService;
     }
 
     @GetMapping("/photos")
     public Collection<Photo> get() {
-        return db.values();
+        return photosService.get();
     }
 
     @GetMapping("/photos/{id}")
     public Photo get(@PathVariable String id) {
-        Photo photo = db.get(id);
+        Photo photo = photosService.get(id);
         if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         return photo;
@@ -43,14 +34,13 @@ public class PhotosController {
 
     @DeleteMapping("/photos/{id}")
     public void delete(@PathVariable String id) {
-        Photo photo = db.remove(id);
+        Photo photo = photosService.remove(id);
         if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/photos")
-    public Photo create(@RequestBody @Valid Photo photo) {
-        photo.setId(UUID.randomUUID().toString());
-        db.put(photo.getId(), photo);
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = photosService.save(file.getOriginalFilename(), file.getContentType(), file.getBytes());
 
         return photo;
     }
