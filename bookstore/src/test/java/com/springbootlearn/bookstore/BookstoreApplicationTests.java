18ba4f23141fd88e56fbd50fe74fd8aeee8d115a
Berkan Serbes <berkan_serbes@hotmail.com>
2023-08-30 23:02:34 +0300

    Initialize bookstore project

---
+++ bookstore/src/test/java/com/springbootlearn/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlearn.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
