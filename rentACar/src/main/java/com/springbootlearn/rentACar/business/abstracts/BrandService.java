b6bb89c7ad2ff5c6bf8a8881439cf627cd04c8f0
Berkan Serbes <berkan_serbes@hotmail.com>
2023-09-05 08:47:38 +0300

    rentACar: Modify business layer

--- rentACar/src/main/java/com/springbootlearn/rentACar/business/abstracts/BrandService.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/business/abstracts/BrandService.java
@@ -1,9 +1,11 @@
 package com.springbootlearn.rentACar.business.abstracts;
 
 import com.springbootlearn.rentACar.entities.concretes.Brand;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface BrandService {
     List<Brand> getAll();
 }
--- rentACar/src/main/java/com/springbootlearn/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/business/concretes/BrandManager.java
@@ -1,4 +1,24 @@
 package com.springbootlearn.rentACar.business.concretes;
 
-public class BrandManager {
+import com.springbootlearn.rentACar.business.abstracts.BrandService;
+import com.springbootlearn.rentACar.dataAccess.abstracts.BrandRepository;
+import com.springbootlearn.rentACar.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BrandManager implements BrandService {
+
+    private BrandRepository brandRepository;
+
+    @Autowired
+    public BrandManager(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+    @Override
+    public List<Brand> getAll() {
+        return brandRepository.getAll();
+    }
 }
