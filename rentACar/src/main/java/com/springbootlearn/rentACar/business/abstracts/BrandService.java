70aea6c2ad244762295ce0551fdcf5787ba4845f
Berkan Serbes <berkan_serbes@hotmail.com>
2023-09-07 20:33:04 +0300

    rentACar: Update business layer

--- rentACar/src/main/java/com/springbootlearn/rentACar/business/abstracts/BrandService.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/business/abstracts/BrandService.java
@@ -1,7 +1,9 @@
 package com.springbootlearn.rentACar.business.abstracts;
 
 import com.springbootlearn.rentACar.business.requests.CreateBrandRequest;
+import com.springbootlearn.rentACar.business.requests.UpdateBrandRequest;
 import com.springbootlearn.rentACar.business.responses.GetAllBrandsResponse;
+import com.springbootlearn.rentACar.business.responses.GetByIdBrandResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,5 +11,8 @@ import java.util.List;
 @Service
 public interface BrandService {
     List<GetAllBrandsResponse> getAll();
+    GetByIdBrandResponse getById(int id);
      void add(CreateBrandRequest createBrandRequest);
+     void update(UpdateBrandRequest updateBrandRequest);
+     void delete(int id);
 }
--- rentACar/src/main/java/com/springbootlearn/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/business/concretes/BrandManager.java
@@ -2,26 +2,28 @@ package com.springbootlearn.rentACar.business.concretes;
 
 import com.springbootlearn.rentACar.business.abstracts.BrandService;
 import com.springbootlearn.rentACar.business.requests.CreateBrandRequest;
+import com.springbootlearn.rentACar.business.requests.UpdateBrandRequest;
 import com.springbootlearn.rentACar.business.responses.GetAllBrandsResponse;
+import com.springbootlearn.rentACar.business.responses.GetByIdBrandResponse;
+import com.springbootlearn.rentACar.core.utilities.mappers.ModelMapperService;
 import com.springbootlearn.rentACar.dataAccess.abstracts.BrandRepository;
 import com.springbootlearn.rentACar.entities.concretes.Brand;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class BrandManager implements BrandService {
     private BrandRepository brandRepository;
+    private ModelMapperService modelMapperService;
 
-    @Autowired
-    public BrandManager(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
     @Override
     public List<GetAllBrandsResponse> getAll() {
         List<Brand> brands = brandRepository.findAll();
+        /*
         List<GetAllBrandsResponse> brandsResponse = new ArrayList<GetAllBrandsResponse>();
 
         for(Brand brand : brands) {
@@ -31,15 +33,42 @@ public class BrandManager implements BrandService {
 
             brandsResponse.add(responseItem);
         }
+        */
+        List<GetAllBrandsResponse> brandsResponse = brands.stream()
+                .map(brand -> this.modelMapperService.forResponse()
+                        .map(brand, GetAllBrandsResponse.class)).collect(Collectors.toList());
 
         return brandsResponse;
     }
 
     @Override
+    public GetByIdBrandResponse getById(int id) {
+        Brand brand = this.brandRepository.findById(id).orElseThrow();
+
+        GetByIdBrandResponse response = this.modelMapperService.forResponse()
+                .map(brand, GetByIdBrandResponse.class);
+
+        return response;
+    }
+
+    @Override
     public void add(CreateBrandRequest createBrandRequest) {
-        Brand brand = new Brand();
-        brand.setName(createBrandRequest.getName());
+        //Brand brand = new Brand();
+        //brand.setName(createBrandRequest.getName());
+
+        Brand brand = this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
 
         this.brandRepository.save(brand);
+    }
+
+    @Override
+    public void update(UpdateBrandRequest updateBrandRequest) {
+        Brand brand = this.modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
+        this.brandRepository.save(brand);
+    }
+
+    @Override
+    public void delete(int id) {
+        this.brandRepository.deleteById(id);
     }
 }
