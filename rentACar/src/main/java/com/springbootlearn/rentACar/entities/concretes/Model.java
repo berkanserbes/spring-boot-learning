56483b6c18bfe3043a9029ab47a6e92f460d9dda
Berkan Serbes <berkan_serbes@hotmail.com>
2023-09-10 16:43:27 +0300

    rentACar: Add Car and Model tables. Establish relationships between tables

--- rentACar/src/main/java/com/springbootlearn/rentACar/entities/concretes/Model.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/entities/concretes/Model.java
@@ -5,13 +5,13 @@ import lombok.*;
 
 import java.util.List;
 
-@Table(name="brands")
+@Table(name = "models")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Brand {
+public class Model {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -20,6 +20,11 @@ public class Brand {
     @Column(name = "name")
     private String name;
 
-    @OneToMany(mappedBy = "brand")
-    private List<Model> models;
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @OneToMany(mappedBy = "model")
+    private List<Car> cars;
+
 }
