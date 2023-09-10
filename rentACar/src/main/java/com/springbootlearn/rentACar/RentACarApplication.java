8ecf05ce3e16228c23bd9c30bc60948c0cc44a87
Berkan Serbes <berkan_serbes@hotmail.com>
2023-09-10 18:13:49 +0300

    rentACar: Add global handle validation exception

--- rentACar/src/main/java/com/springbootlearn/rentACar/RentACarApplication.java
+++ rentACar/src/main/java/com/springbootlearn/rentACar/RentACarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,6 +25,15 @@ public class RentACarApplication {
 	public ProblemDetails handleBusinessException(BusinessException businessException) {
 		ProblemDetails problemDetails = new ProblemDetails();
 		problemDetails.setMessage(businessException.getMessage());
+
+		return problemDetails;
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ProblemDetails handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
+		ProblemDetails problemDetails= new ProblemDetails();
+		problemDetails.setMessage("Validation Exception");
 
 		return problemDetails;
 	}
